8e9eed3e3f972573a4d552dfb29108b94c7cdb96
Niels Hempelmann <89600300+hemniels@users.noreply.github.com>
2024-06-25 15:19:08 +0200

    dataloader module refactor

--- dataloader/dataloader.c
+++ dataloader/dataloader.c
@@ -18,38 +18,46 @@ int load_fasta_file(const char *file_path, Sequence **sequences, int *seq_count)
     Sequence *seq_array = malloc(sizeof(Sequence) * 100);  // Startgröße
     int count = 0;
     int header = 1; // Flag to identify header lines
+    char *buffer_end = buffer + BUFFER_SIZE;
+    char *start, *end;
 
     while (1) {
         size_t bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
         if (bytes_read == 0) break;
 
-        for (size_t i = 0; i < bytes_read; i++) {
-            if (buffer[i] == '>') {
+        start = buffer;
+        end = buffer;
+
+        while (end < buffer + bytes_read) {
+            if (*end == '>') {
                 if (header == 0 && current_seq_len > 0) {
                     // Aktuelle Sequenz speichern
-                    seq_array[count - 1].sequence = strdup(current_seq);
+                    seq_array[count - 1].sequence = strndup(current_seq, current_seq_len);
                     current_seq_len = 0;
                 }
                 // Neue Sequenz starten
-                size_t id_start = i + 1;
-                while (i < bytes_read && buffer[i] != '\n') i++;
-                buffer[i] = '\0';
-                seq_array[count].id = strdup(&buffer[id_start]);
+                start = end + 1;
+                while (*end != '\n' && end < buffer + bytes_read) end++;
+                seq_array[count].id = strndup(start, end - start);
                 count++;
                 header = 1;
                 if (count % 100 == 0) {
                     seq_array = realloc(seq_array, sizeof(Sequence) * (count + 100));
                 }
-            } else if (buffer[i] != '\n' && buffer[i] != '\r') {
-                current_seq[current_seq_len++] = buffer[i];
-                current_seq[current_seq_len] = '\0';
+            } else if (*end != '\n' && *end != '\r') {
+                current_seq[current_seq_len++] = *end;
                 header = 0;
             }
+            end++;
         }
+
+        // Restliche Pufferinhalte für den nächsten Durchlauf aufbewahren
+        size_t remaining = buffer + bytes_read - start;
+        memmove(buffer, start, remaining);
     }
 
     if (current_seq_len > 0) {
-        seq_array[count - 1].sequence = strdup(current_seq);
+        seq_array[count - 1].sequence = strndup(current_seq, current_seq_len);
     }
 
     free(buffer);
